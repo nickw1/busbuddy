603b644c3ea4649b7f3e7d41358101be103872d5
Nick Whitelegg <nickw4426@gmail.com>
2025-03-18 10:49:36 +0100

    Further modification to workings analyser

--- src/analyse/workings.py
+++ src/analyse/workings.py
@@ -18,7 +18,7 @@ enddate=sys.argv[2]
 
 with psycopg.connect(f"dbname={os.environ.get('DB_NAME')} user={os.environ.get('DB_USER')}", row_factory=dict_row) as conn:
     with conn.cursor() as cur:
-        cur.execute("SELECT j.route, j.deptime, j.direction, log.journeyid, d.date, log.vehicle_today FROM daily_journey_log log INNER JOIN dates d ON d.id=log.dateid INNER JOIN journeys j ON j.id=log.journeyid WHERE extract(dow from d.date::timestamp) BETWEEN 1 AND 5 AND d.date BETWEEN %s AND %s ORDER BY log.dateid, log.vehicle_today, j.deptime", (startdate, enddate))
+        cur.execute("SELECT j.route, j.deptime, j.direction, j.block_ref, log.journeyid, d.date, log.vehicle_today FROM daily_journey_log log INNER JOIN dates d ON d.id=log.dateid INNER JOIN journeys j ON j.id=log.journeyid WHERE extract(dow from d.date::timestamp) BETWEEN 1 AND 5 AND d.date BETWEEN %s AND %s ORDER BY log.dateid, log.vehicle_today, j.deptime", (startdate, enddate))
         results = cur.fetchall()
         last_vehicle = None
         key = None
@@ -27,7 +27,7 @@ with psycopg.connect(f"dbname={os.environ.get('DB_NAME')} user={os.environ.get('
             dates.add(date)
             if last_vehicle != result["vehicle_today"]:
                 #key = f"{result['route']}:{result['deptime'].hour*120+result['deptime'].minute*2+(1 if result['direction'][0] == 'i' else 0)}"
-                key = f"{result['route']}:{result['journeyid']}"
+                key = f"{result['block_ref'] if result['block_ref'] is not None else result['route']}:{result['journeyid']}"
                 last_vehicle = result["vehicle_today"]
                 if key not in workings:
                     workings[key] = {}
@@ -43,30 +43,32 @@ with psycopg.connect(f"dbname={os.environ.get('DB_NAME')} user={os.environ.get('
 
 #            print(f"{journey['journeyid']} {journey['route']} {journey['deptime']}")
 
-threshold=0.5
-counted={}
-print(f"Number of dates: {len(dates)}")
-ndates = len(dates)
-if ndates < 5:
-    print("Error: a minimum of 5 dates is required.")
-else:
-    for wkey in workings:
-        operated_dates = len(workings[wkey])
-        print(f"WORKING: {wkey} operated on {operated_dates} dates out of {ndates}.")
-        if operated_dates / ndates >= threshold:         
-            print("*** Counting ***")
-            for date in workings[wkey]:
-                print(f"Date {date}: Vehicle {workings[wkey][date]['vehicle']}")
-                for journey in workings[wkey][date]["journeys"]:
-                    freqs[wkey][journey['journeyid']] = 1 if journey['journeyid'] not in freqs[wkey] else freqs[wkey][journey['journeyid']] + 1
+        threshold=0.5
+        counted={}
+        print(f"Number of dates: {len(dates)}")
+        ndates = len(dates)
+        if ndates < 5:
+            print("Error: a minimum of 5 dates is required.")
+        else:
+            for wkey in workings:
+                operated_dates = len(workings[wkey])
+                print(f"WORKING: {wkey} operated on {operated_dates} dates out of {ndates}.")
+                if operated_dates / ndates >= threshold:         
+                    print("*** Counting ***")
+                    for date in workings[wkey]:
+                        print(f"Date {date}: Vehicle {workings[wkey][date]['vehicle']}")
+                        for journey in workings[wkey][date]["journeys"]:
+                            freqs[wkey][journey['journeyid']] = 1 if journey['journeyid'] not in freqs[wkey] else freqs[wkey][journey['journeyid']] + 1
 
-    for fkey in freqs:
-        for journeyid in freqs[fkey]:
-            if freqs[fkey][journeyid] / ndates >= threshold:
-                if fkey not in counted:
-                    counted[fkey] = []
-                counted[fkey].append(journeyid)
+            for fkey in freqs:
+                for journeyid in freqs[fkey]:
+                    if freqs[fkey][journeyid] / ndates >= threshold:
+                        if fkey not in counted:
+                            counted[fkey] = []
+                        counted[fkey].append(journeyid)
 
-print("***** COUNTED WORKINGS *****")
-for working in counted:
-    print(f"{working} {counted[working]}")
+        print("***** COUNTED WORKINGS *****")
+        for working in counted:
+            print(f"{working} {counted[working]}")
+            for journey in counted[working]:
+                cur.execute("UPDATE journeys SET analysed_block_ref=%s WHERE id=%s",(working, journey))

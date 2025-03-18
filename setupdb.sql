ab3f98f350ba8ce9faff211dd0475ebdafa9216e
Nick Whitelegg <nickw4426@gmail.com>
2025-03-18 16:26:35 +0100

    further development of workings analyser script

--- setupdb.sql
+++ setupdb.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS terms;
 DROP TABLE IF EXISTS journeystops;
---DROP TABLE IF EXISTS stops;
+DROP TABLE IF EXISTS stops;
 DROP TABLE IF EXISTS journeys;
 DROP TABLE IF EXISTS tt_revisions;
 DROP TABLE IF EXISTS education_authorities;
@@ -11,7 +11,7 @@ CREATE TABLE tt_revisions(id serial PRIMARY KEY, dataset_id INT, date DATE);
 
 CREATE TABLE journeys(id serial PRIMARY KEY, block_ref VARCHAR(255), origin VARCHAR(255), destination VARCHAR(255), operator_name VARCHAR(255), route VARCHAR(255), deptime TIME WITHOUT TIME ZONE, run_days VARCHAR(255), termdays INT, holidays INT, direction VARCHAR(255), vehicle_today VARCHAR(255), rev_id INT, FOREIGN KEY (termdays) REFERENCES education_authorities(id), FOREIGN KEY (holidays) REFERENCES education_authorities(id), FOREIGN KEY (rev_id) REFERENCES tt_revisions(id));
 
---CREATE TABLE stops (id serial PRIMARY KEY, atco_code VARCHAR(255), naptan_code VARCHAR(255), common_name VARCHAR(255), lat FLOAT, lon FLOAT, street VARCHAR(255), locality_name VARCHAR(255), parent_locality_name VARCHAR(255));
+CREATE TABLE stops (id serial PRIMARY KEY, atco_code VARCHAR(255), naptan_code VARCHAR(255), common_name VARCHAR(255), lat FLOAT, lon FLOAT, street VARCHAR(255), locality_name VARCHAR(255), parent_locality_name VARCHAR(255));
 
 CREATE TABLE journeystops (id serial PRIMARY KEY, journeyid INT, stopid INT, reltime INT, FOREIGN KEY (journeyid) REFERENCES journeys(id), FOREIGN KEY (stopid) REFERENCES stops(id));
 
--- src/analyse/workings.py
+++ src/analyse/workings.py
@@ -1,6 +1,7 @@
 import psycopg
 import os
 import dotenv
+import sys
 from psycopg.rows import dict_row
 
 dotenv.load_dotenv()
@@ -8,10 +9,16 @@ dotenv.load_dotenv()
 workings={}
 freqs={}
 dates=set()
+if len(sys.argv) < 3:
+    print(f"Usage: python3 {sys.argv[0]} startdate enddate")
+    sys.exit(1)
+
+startdate=sys.argv[1]
+enddate=sys.argv[2]
 
 with psycopg.connect(f"dbname={os.environ.get('DB_NAME')} user={os.environ.get('DB_USER')}", row_factory=dict_row) as conn:
     with conn.cursor() as cur:
-        cur.execute("SELECT j.route, j.deptime, j.direction, log.journeyid, d.date, log.vehicle_today FROM daily_journey_log log INNER JOIN dates d ON d.id=log.dateid INNER JOIN journeys j ON j.id=log.journeyid WHERE extract(dow from d.date::timestamp) BETWEEN 1 AND 5 ORDER BY log.dateid, log.vehicle_today, j.deptime")
+        cur.execute("SELECT j.route, j.deptime, j.direction, log.journeyid, d.date, log.vehicle_today FROM daily_journey_log log INNER JOIN dates d ON d.id=log.dateid INNER JOIN journeys j ON j.id=log.journeyid WHERE extract(dow from d.date::timestamp) BETWEEN 1 AND 5 AND d.date BETWEEN %s AND %s ORDER BY log.dateid, log.vehicle_today, j.deptime", (startdate, enddate))
         results = cur.fetchall()
         last_vehicle = None
         key = None
@@ -36,25 +43,30 @@ with psycopg.connect(f"dbname={os.environ.get('DB_NAME')} user={os.environ.get('
 
 #            print(f"{journey['journeyid']} {journey['route']} {journey['deptime']}")
 
+threshold=0.5
 counted={}
 print(f"Number of dates: {len(dates)}")
 ndates = len(dates)
 if ndates < 5:
     print("Error: a minimum of 5 dates is required.")
 else:
-    for key in workings:
-#    print(f"WORKING: {key}")
-        for date in workings[key]:
-#        print(f"Date {date}: Vehicle {workings[key][date]['vehicle']}")
-            for journey in workings[key][date]["journeys"]:
-                freqs[key][journey['journeyid']] = 1 if journey['journeyid'] not in freqs[key] else freqs[key][journey['journeyid']] + 1
-    threshold=0.5
-    for key in freqs:
-        if len(freqs[key]) / ndates >= threshold:
-            for journeyid in freqs[key]:
-                if freqs[key][journeyid] / len(freqs[key]) >= threshold:
-                    if key not in counted:
-                        counted[key] = []
-                    counted[key].append(journeyid)
+    for wkey in workings:
+        operated_dates = len(workings[wkey])
+        print(f"WORKING: {wkey} operated on {operated_dates} dates out of {ndates}.")
+        if operated_dates / ndates >= threshold:         
+            print("*** Counting ***")
+            for date in workings[wkey]:
+                print(f"Date {date}: Vehicle {workings[wkey][date]['vehicle']}")
+                for journey in workings[wkey][date]["journeys"]:
+                    freqs[wkey][journey['journeyid']] = 1 if journey['journeyid'] not in freqs[wkey] else freqs[wkey][journey['journeyid']] + 1
 
-print(counted)
+    for fkey in freqs:
+        for journeyid in freqs[fkey]:
+            if freqs[fkey][journeyid] / ndates >= threshold:
+                if fkey not in counted:
+                    counted[fkey] = []
+                counted[fkey].append(journeyid)
+
+print("***** COUNTED WORKINGS *****")
+for working in counted:
+    print(f"{working} {counted[working]}")
